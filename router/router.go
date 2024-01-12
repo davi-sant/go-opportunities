f1ca5d977bc24676ae028373f5e4cc3c2d994492
Davi <davisantosdias@protonmail.com>
2024-01-12 01:02:23 -0300

    feat: creating and separating routes.

--- router/router.go
+++ router/router.go
@@ -5,12 +5,6 @@ import "github.com/gin-gonic/gin"
 // Inicia esculta na porta :8080
 func Routes() {
 	router := gin.Default()
-
-	router.GET("api/teste/v0.0.1/opportunists", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"error":   false,
-			"message": "Accept:code",
-		})
-	})
+	initializeRoutes(router)
 	router.Run()
 }
